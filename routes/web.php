f986f9dace6bceb5102052f19c107c881a06e6c4
ahmy1996404 <a.hamodys@yahoo.com>
2022-03-07 02:12:54 +0200

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Http\Controllers\ContactController;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/home', function () {
+    echo 'this is home page';
+});
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact' , [ContactController::class , 'index'])->name('contact');
+
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    //$users = User::all();
+    $users = DB::table('users')->get();
+    return view('dashboard' , compact('users'));
+})->name('dashboard');
