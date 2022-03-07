395fb212f54dfe7e2440ea91c42ee78ebe0fe761
ahmy1996404 <a.hamodys@yahoo.com>
2022-03-07 03:26:04 +0200

    Eloquent ORM Insert Data

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 
 class CategoryController extends Controller
 {
@@ -23,13 +27,27 @@ class CategoryController extends Controller
      */
     public function AddCat(Request $request)
     {
-        $validated = $request->validate([
+        $validated = $request->validate(
+            [
             'category_name' => 'required|unique:categories|max:255',
-        ],
-    [
-        'category_name.required'=>'Please insert category name',
-        'category_name.max'=>'Category must be less than 255 chars'
+            ],
+        [
+            'category_name.required'=>'Please insert category name',
+            'category_name.max'=>'Category must be less than 255 chars'
+            ]
+        );
+        Category::insert([
+            'category_name' => $request->category_name,
+            'user_id' => Auth::user()->id,
+            'created_at' => Carbon::now()
+        ]);
 
-    ]);
+        // $category = new Category();
+        // $category->category_name = $request->category_name;
+        // $category->user_id = Auth::user()->id;
+        // $category->save();
+
+        return Redirect()->back()->with('success','Category Inserted Successfull');
+
     }
 }
--- database/migrations/2022_03_07_000706_create_categories_table.php
+++ database/migrations/2022_03_07_000706_create_categories_table.php
@@ -16,7 +16,7 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->integer('user_id');
-            $table->integer('category_name');
+            $table->string('category_name');
             $table->timestamps();
             $table->softDeletes();
 
