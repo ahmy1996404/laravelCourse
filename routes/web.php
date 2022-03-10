2075acb2f3d33d5c117c8bdcb1fdf9112c5a6cf5
ahmy1996404 <a.hamodys@yahoo.com>
2022-03-10 05:20:39 +0200

    Email Verify in Laravel

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
 
 Route::get('/', function () {
     return view('welcome');
