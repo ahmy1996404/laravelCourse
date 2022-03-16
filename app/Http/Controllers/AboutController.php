5fe02ef2225159669e82d3375dc97763fe7cabf3
ahmy1996404 <a.hamodys@yahoo.com>
2022-03-16 05:48:35 +0200

    Setup Home page About Section Part 4

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -11,7 +11,7 @@ class AboutController extends Controller
     public function HomeAbout()
     {
         $homeabout = HomeAbout::latest()->get();
-        return view('admin.home.index',compact('homeabout'));
+        return view('admin.home.index', compact('homeabout'));
     }
     public function AddAbout()
     {
@@ -20,25 +20,30 @@ class AboutController extends Controller
     public function StoreAbout(Request $request)
     {
         HomeAbout::insert([
-            'title' =>$request -> title,
-            'short_dis' =>$request -> title,
-            'log_dis' =>$request -> log_dis,
-            'created_at'=> Carbon::now()
+            'title' => $request->title,
+            'short_dis' => $request->short_dis,
+            'log_dis' => $request->log_dis,
+            'created_at' => Carbon::now()
         ]);
-        return redirect()->route('home.about')->with('success','About inserted successfully');
+        return redirect()->route('home.about')->with('success', 'About inserted successfully');
     }
     public function EditAbout($id)
     {
         $homeabout = HomeAbout::find($id);
-        return view('admin.home.edit',compact('homeabout'));
+        return view('admin.home.edit', compact('homeabout'));
     }
-    public function UpdateAbout(Request $request,$id)
+    public function UpdateAbout(Request $request, $id)
     {
         $update = HomeAbout::find($id)->update([
             'title' => $request->title,
-            'short_dis' => $request->title,
+            'short_dis' => $request->short_dis,
             'log_dis' => $request->log_dis,
         ]);
         return redirect()->route('home.about')->with('success', 'About updated successfully');
+    }
+    public function DeleteAbout($id)
+    {
+        $delete = HomeAbout::find($id)->delete();
+        return redirect()->back()->with('success', 'About deleted successfully');
     }
 }
